c1b24440c5516d55b139a7dbaf64fdc5acc42085
Tretchian <safonovila7@gmail.com>
2023-11-28 15:16:58 +0300

    Пустоножка (кродётся)
    WIP префаб пустоножки с её базовой механикой перемещения

--- Assets/Prefabs/Emptypede/Emptypede_headAI.cs
+++ Assets/Prefabs/Emptypede/Emptypede_headAI.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Emptypede_headAI : MonoBehaviour
@@ -8,21 +7,23 @@ public class Emptypede_headAI : MonoBehaviour
 
     private Health health;
     private Rigidbody2D rb;
-    private GameObject[] bodyparts;
+    public GameObject[] bodyparts;
     [SerializeField] private GameObject body_prefab;
-    [SerializeField] private int length = 3;
+    [SerializeField] private int length = 5;
 
     private Vector2 movement_direction = Vector2.left;
+    
     void Start()
     {
         health = GetComponent<Health>();
         rb = GetComponent<Rigidbody2D>();
-
+        bodyparts = new GameObject[length];
+      
     }
 
     void FixedUpdate()
     {
-        transform.Translate(movement_direction.normalized*_speed*Time.deltaTime,transform);
+     
     }
 
     private void CreateBodyPart(GameObject target_object,int id)
@@ -32,7 +33,7 @@ public class Emptypede_headAI : MonoBehaviour
         body_script.speed = _speed;
         body_script.next_bodypart = target_object;
         body_script._id = id;
-        bodyparts.SetValue(new_part, id);
+        bodyparts[id] = new_part;
         Instantiate(new_part);
     }
 }
