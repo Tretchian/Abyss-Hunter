fe20c929a24d37ed9fc1aeb70cbd786841b1ec95
Tretchian <safonovila7@gmail.com>
2023-11-28 07:16:07 +0300

    Скрипт пустоножки
    Утро начинается с коммита...

--- Assets/Prefabs/Emptypede/Emptypede_body.cs
+++ Assets/Prefabs/Emptypede/Emptypede_body.cs
@@ -1,8 +1,23 @@
 
 using UnityEngine;
 
-public class Emptypede_body : ScriptableObject
+public class Emptypede_body : MonoBehaviour
 {
-    private int _id;
+    public int _id;
     private Health health;
+    public GameObject next_bodypart;
+    public float speed = 1f;
+    private Vector2 movement;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (next_bodypart.Equals(null)) return;
+        movement = next_bodypart.transform.position - transform.position;
+        transform.Translate(movement.normalized * Time.deltaTime * speed);
+    }
 }
--- Assets/Prefabs/Emptypede/Emptypede_headAI.cs
+++ Assets/Prefabs/Emptypede/Emptypede_headAI.cs
@@ -8,16 +8,31 @@ public class Emptypede_headAI : MonoBehaviour
 
     private Health health;
     private Rigidbody2D rb;
+    private GameObject[] bodyparts;
+    [SerializeField] private GameObject body_prefab;
+    [SerializeField] private int length = 3;
 
     private Vector2 movement_direction = Vector2.left;
     void Start()
     {
         health = GetComponent<Health>();
         rb = GetComponent<Rigidbody2D>();
+
     }
 
     void FixedUpdate()
     {
         transform.Translate(movement_direction.normalized*_speed*Time.deltaTime,transform);
+    }
+
+    private void CreateBodyPart(GameObject target_object,int id)
+    {
+        GameObject new_part = body_prefab;
+        Emptypede_body body_script = new_part.GetComponent<Emptypede_body>();
+        body_script.speed = _speed;
+        body_script.next_bodypart = target_object;
+        body_script._id = id;
+        bodyparts.SetValue(new_part, id);
+        Instantiate(new_part);
     }
 }
