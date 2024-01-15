665b7626445c61554247b4638fa3e512a70b2c01
Danila <danila-zol@list.ru>
2024-01-16 00:45:17 +0300

    Level generation prototype

---
+++ Assets/Scripts/RoomTemplates.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject[] bottomRooms;
+    public GameObject[] topRooms;
+    public GameObject[] leftRooms;
+    public GameObject[] rightRooms;
+}
