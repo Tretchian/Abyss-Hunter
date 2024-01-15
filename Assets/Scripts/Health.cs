94cddcef5d5614696ca640f21c78b15f923d2e93
Tretchian <safonovila7@gmail.com>
2024-01-15 22:19:11 +0300

    Revert "Merge branch 'Tretchian-works' into Development"
    This reverts commit 2e9eb66cb3243e56ca395f7297a79ac9a49287e6, reversing
    changes made to 237e9b636fb583d448e964c21cd2b6f38f9a35d9.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -32,18 +32,8 @@ public class Health : MonoBehaviour
         }
         else {
             _currentHealth -= damage;
-<<<<<<< HEAD
-<<<<<<< HEAD
-            OnTakenDamage.Invoke(transform.gameObject);
-            StartCoroutine(becomeInvulnerable());
-=======
-            //OnHealthChange.Invoke();
-            StartCoroutine(becomeInvulnerable()); 
->>>>>>> Arsenii
-=======
             OnHealthChange.Invoke();
             StartCoroutine(becomeInvulnerable()); 
->>>>>>> parent of 83dc4af (Проклятый банан)
         }
         
     }
--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -26,7 +26,6 @@ public class PlayerStats: MonoBehaviour
         player_center = (Vector2)transform.position + GetComponent<BoxCollider2D>().offset;
         pickup();
     }
-    public float getAttackDmg => playerStats._attackdmg;
     void recount()
     {
         playerStats = new Stats();
@@ -51,7 +50,6 @@ public class PlayerStats: MonoBehaviour
         }
         recount();
     }
-
 
     private void OnDrawGizmos()
     {
