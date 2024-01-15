992dd33fa1e65dc6ba25607e8f6f4c18dc390843
tini-yu <i.novoselova.sas@gmail.com>
2024-01-16 01:59:38 +0500

    added animations for player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,11 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 movement_direction;
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
+    public Animator animator;
+    public bool left;
+    public bool right;
+    public bool up;
+    public bool down;
     
     public float speed = 1f;
     void Start()
@@ -14,13 +19,42 @@ public class PlayerMovement : MonoBehaviour
     }
     void Update()
     {
-        
+        var direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        var right = false; var up = false; var down = false; var left = false;
+        if (angle <= 135 & angle >=45)
+        {
+            up = true;
+        }
+        if (angle <= -135 || angle > 135)
+        {
+            left = true;
+        }
+        if (angle > -135 & angle <= -45)
+        {
+            down = true;
+        }
+        if (angle > -45 & angle < 45)
+        {
+            right = true;
+        }
+        animator.SetBool("up", up);
+        animator.SetBool("down", down);
+        animator.SetBool("right", right);
+        animator.SetBool("left", left);
     }
     private void FixedUpdate()
     {
         movement_direction.x = Input.GetAxis("Horizontal");
         movement_direction.y = Input.GetAxis("Vertical");
-        sprite.flipX = movement_direction.x < 0;
+        //sprite.flipX = movement_direction.x < 0;
         transform.Translate(movement_direction.normalized * speed * Time.deltaTime, rb.transform);
+
+        var move = false;
+        if (movement_direction.x != 0 || movement_direction.y != 0)
+        {
+            move = true;
+        }
+        animator.SetBool("move", move);
     }
 }
